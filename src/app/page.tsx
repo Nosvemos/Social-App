d1ab1bb668107ab8706c57db0631b324397e88be
Samet Ozturk <53067521+Nosvemos@users.noreply.github.com>
2025-03-24 15:48:17 +0300

    Add shadcnUI and theme toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,24 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import { Button } from '@/components/ui/button'
+import ThemeToggle from '@/components/ThemeToggle'
 
 export default function Home() {
   return (
-    <div>
+    <div className='m-4'>
       <SignedOut>
-        <SignInButton mode='modal'/>
+        <SignInButton mode='modal'>
+          <Button>
+            Sign in
+          </Button>
+        </SignInButton>
       </SignedOut>
       <SignedIn>
         <UserButton />
       </SignedIn>
+
+      <ThemeToggle />
+
+      <Button variant='secondary'>Click me!</Button>
     </div>
   );
 }
